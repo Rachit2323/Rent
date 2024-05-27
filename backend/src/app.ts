05ffc8785bdb9051641e9b19caa28e1cf2f8d8fa
Rachit2323 <atreyrachit23@gmail.com>
2024-05-27 23:20:13 +0530

    deploy

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors({
     origin: [
         'http://localhost:3000',
-        'https://rent-ezbm.vercel.app/buyer'
+        'https://rent-ezbm.vercel.app'
     ]
 }));
 
